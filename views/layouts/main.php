24d833c282d7d65796dd44fb7787a687354fc7a1
Bogdan <shmatovb@mail.ru>
2020-09-30 23:50:18 +0600

    Added new item My form in menu navigation

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -42,6 +42,7 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             ['label' => 'Say', 'url' => ['/site/say']],
+            ['label' => 'My form', 'url' => ['/site/myform']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
